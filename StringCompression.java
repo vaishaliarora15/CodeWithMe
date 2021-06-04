4ee5982190120d8a1f4eddae24619d4b6428f9c7
Vaishali Arora <71548078+vaishaliarora15@users.noreply.github.com>
2021-06-04 15:36:51 +0530

    Leetcode 443

---
+++ StringCompression.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int i = 0;
+        int newlen = 0;
+        while(i<n){
+            int j = i;
+            while(j<n && chars[i] == chars[j]){
+                j++;
+            }
+            chars[newlen++] = chars[i];
+            if(j-i > 1){
+                String count = j-i + "";
+                for(char c : count.
+                    chars[newlen++] = c;
+                }
+            }
+            i = j;
+        }
+        return newlen;
+    }
+}
